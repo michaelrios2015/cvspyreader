9f2ed9db1cf1c19f0d5b827a837e7a79976cf651
Michael Rios <michael.rios093@gmail.com>
2022-01-03 21:05:19 -0500

    reading in spiting out data

---
+++ test.py
@@ -0,0 +1,47 @@
+import csv
+
+with open('FNM_MF_202112.txt', newline='') as csvfile:
+    # data = list(csv.reader(csvfile, delimiter='|'))
+    reader = csv.DictReader(csvfile, delimiter='|')
+
+    head = []
+    body = []
+
+    for row in reader:
+        head.append([row["Prefix"], row["Security Identifier"], row["CUSIP"]])
+        body.append([row["CUSIP"], row["Security Factor"], row["WA Issuance Interest Rate"], row["WA Loan Age"], row["WA Issuance Remaining Months to Maturity"]])
+
+
+
+headfields = ["Prefix", "Security Identifier", "CUSIP"]
+
+with open('head.cvs', 'w', newline='') as csvfile: 
+    # creating a csv writer object 
+    csvwriter = csv.writer(csvfile) 
+        
+    # writing the fields 
+    csvwriter.writerow(headfields) 
+        
+    # writing the data rows 
+    csvwriter.writerows(head)
+
+
+
+bodyFields = ["CUSIP", "Security Factor", "WA Issuance Interest Rate", "WA Loan Age", "WA Issuance Remaining Months to Maturity"]
+
+with open('body.cvs', 'w', newline='') as csvfile: 
+    # creating a csv writer object 
+    csvwriter = csv.writer(csvfile) 
+        
+    # writing the fields 
+    csvwriter.writerow(bodyFields) 
+        
+    # writing the data rows 
+    csvwriter.writerows(body)
+
+
+
+
+
+
+
