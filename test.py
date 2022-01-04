cd3d6d476ae28d087412dff49e6d889f49b947ce
Michael Rios <michael.rios093@gmail.com>
2022-01-04 18:06:25 -0500

    started platinum reader

--- test.py
+++ test.py
@@ -7,37 +7,41 @@ with open('data/FNM_MF_202112.txt', newline='') as csvfile:
     head = []
     body = []
 
+    # for row in reader:
+    #     head.append([row["Prefix"], row["Security Identifier"], row["CUSIP"]])
+    #     body.append([row["CUSIP"], row["Security Factor"], row["WA Issuance Interest Rate"], row["WA Loan Age"], row["WA Issuance Remaining Months to Maturity"]])
+
     for row in reader:
-        head.append([row["Prefix"], row["Security Identifier"], row["CUSIP"]])
-        body.append([row["CUSIP"], row["Security Factor"], row["WA Issuance Interest Rate"], row["WA Loan Age"], row["WA Issuance Remaining Months to Maturity"]])
+        print(row)
+        break    
 
 
 
-headfields = ["Prefix", "Security Identifier", "CUSIP"]
+# headfields = ["Prefix", "Security Identifier", "CUSIP"]
 
-with open('data/head.cvs', 'w', newline='') as csvfile: 
-    # creating a csv writer object 
-    csvwriter = csv.writer(csvfile) 
+# with open('data/head.cvs', 'w', newline='') as csvfile: 
+#     # creating a csv writer object 
+#     csvwriter = csv.writer(csvfile) 
         
-    # writing the fields 
-    csvwriter.writerow(headfields) 
+#     # writing the fields 
+#     csvwriter.writerow(headfields) 
         
-    # writing the data rows 
-    csvwriter.writerows(head)
+#     # writing the data rows 
+#     csvwriter.writerows(head)
 
 
 
-bodyFields = ["CUSIP", "Security Factor", "WA Issuance Interest Rate", "WA Loan Age", "WA Issuance Remaining Months to Maturity"]
+# bodyFields = ["CUSIP", "Security Factor", "WA Issuance Interest Rate", "WA Loan Age", "WA Issuance Remaining Months to Maturity"]
 
-with open('data/body.cvs', 'w', newline='') as csvfile: 
-    # creating a csv writer object 
-    csvwriter = csv.writer(csvfile) 
+# with open('data/body.cvs', 'w', newline='') as csvfile: 
+#     # creating a csv writer object 
+#     csvwriter = csv.writer(csvfile) 
         
-    # writing the fields 
-    csvwriter.writerow(bodyFields) 
+#     # writing the fields 
+#     csvwriter.writerow(bodyFields) 
         
-    # writing the data rows 
-    csvwriter.writerows(body)
+#     # writing the data rows 
+#     csvwriter.writerows(body)
 
 
 
@@ -45,3 +49,9 @@ with open('data/body.cvs', 'w', newline='') as csvfile:
 
 
 
+# 
+# looks like we will need this hopefully it is orginal face 
+# Issuance Investor Security UPB': '26260359.00'
+
+# november 2023 I think 
+# 'Maturity Date': '112023',
