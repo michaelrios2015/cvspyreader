a115492b102930f1ff8ef84ab3299ffe322d4f14
Michael Rios <michael.rios093@gmail.com>
2022-01-23 21:39:26 -0500

    will call process data for web and load it into cmos database but need to show if any have nagative float

---
+++ data_for_web.py
@@ -0,0 +1,67 @@
+import csv
+import psycopg2
+
+# connects to database
+conn = psycopg2.connect(
+    database="cmos_builder", user='postgres',
+    password='JerryPine', host='localhost', port='5432'
+)
+
+# UPDATE CURRENT MONTH AND FED DATE the rest should be good
+
+currentmonth = "'" + '2021-11-01' + "'"
+feddate = "'" + '2022-01-12' + "'"
+
+# connecting to database
+# probably don't need to
+conn.autocommit = True
+cursor = conn.cursor()
+
+sql = '''
+
+Call processdataforweb(''' + currentmonth + ''', ''' + feddate + ''');
+
+'''
+
+# print(sql)
+
+cursor.execute(sql)
+
+conn.commit()
+conn.close()
+
+
+conn = psycopg2.connect(
+    database="cmos", user='postgres',
+    password='JerryPine', host='localhost', port='5432'
+)
+
+conn.autocommit = True
+cursor = conn.cursor()
+
+sql = ''' TRUNCATE ginnies; '''
+cursor.execute(sql)
+
+
+csv_file_name = 'C:/Users/Public/ginnieplatswithcurrfloatM'
+sql = "COPY ginnies FROM STDIN DELIMITER ',' CSV HEADER"
+cursor.copy_expert(sql, open(csv_file_name, "r"))
+
+csv_file_name = 'C:/Users/Public/ginnieplatswithcurrfloatX'
+sql = "COPY ginnies FROM STDIN DELIMITER ',' CSV HEADER"
+cursor.copy_expert(sql, open(csv_file_name, "r"))
+
+csv_file_name = 'C:/Users/Public/poolswithcurrfloatM'
+sql = "COPY ginnies FROM STDIN DELIMITER ',' CSV HEADER"
+cursor.copy_expert(sql, open(csv_file_name, "r"))
+
+csv_file_name = 'C:/Users/Public/poolswithcurrfloatX'
+sql = "COPY ginnies FROM STDIN DELIMITER ',' CSV HEADER"
+cursor.copy_expert(sql, open(csv_file_name, "r"))
+
+
+conn.commit()
+conn.close()
+
+
+# this seems to work not sure if i really need to make the cvs file... but well i know how to do it this way so
--- platcolls.py
+++ platcolls.py
@@ -1,5 +1,4 @@
 import csv
-import sys
 
 #  So I seem to be able to read in the platcolls just fine
 # but inserting it might be a bit trickier.. that will be seen in laoding scripts section
@@ -27,12 +26,6 @@ for row in Lines:
     data.append([row[0:9], row[19:25], row[25:26],
                 row[53:68], row[79:80], date])
 
-    # console.log(csvMonthCollateral[i])
-    # const cusip = csvMonthCollateral[i][0].slice(0, 9);
-    # const poolname = csvMonthCollateral[i][0].slice(19, 25);
-    # const indicator = csvMonthCollateral[i][0].slice(25, 26);
-    # const faceinplatinum = csvMonthCollateral[i][0].slice(53, 68);
-    # const active = csvMonthCollateral[i][0].slice(79, 80);
 
 # so seems to work would put in a temp table then switch to
 
